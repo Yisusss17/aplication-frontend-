6b870c6fabf15b71d4af390f43d6e5683432b1bb
ricardo5salinas <ricardoasm1999@gmail.com>
2025-04-12 13:47:40 -0400

    Eliminacion del NEW

--- src/_nav.js
+++ src/_nav.js
@@ -22,10 +22,7 @@ const _nav = [
     name: 'Dashboard',
     to: '/dashboard',
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
+    
   },
   {
     component: CNavTitle,
@@ -404,10 +401,7 @@ const _nav = [
     name: 'Widgets',
     to: '/widgets',
     icon: <CIcon icon={cilCalculator} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
+    
   },
   {
     component: CNavTitle,
